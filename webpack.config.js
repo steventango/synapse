87bb961c7c69b07e0eaabf1587a29754ae14c047
Steven Tang <steventango@users.noreply.github.com>
2021-05-19 22:26:55 -0600

    Add devServer configuration

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,13 @@ module.exports = [{
       },
     ]
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    host: '127.0.0.1',
+    port: 8080,
+    open: false
+  },
   resolve: {
     extensions: ['.ts', '.js'],
   },
